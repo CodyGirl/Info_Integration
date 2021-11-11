91d3567c558030255ccf35626bff8a2c9ca281e1
CodyGirl <nistha.bhawsinka@gmail.com>
2021-11-11 12:55:04 +0100

    extracter + loader for university data

--- Python_Backend/pythonFiles/main.py
+++ Python_Backend/pythonFiles/main.py
@@ -1,23 +1,24 @@
 from flask import Flask,json
 from flask_cors import CORS
-import pyodbc
 import mysql.connector
 from mysql.connector import Error
+import pandas as pd
+import math
 
 app = Flask(__name__)
 database = 'info_integration' 
 username = 'root' 
 password = 'mysql' 
-# conn = pyodbc.connect('Driver={ODBC Driver 17 for SQL Server};Sever = DESKTOP-3H5IL08;Database = info_integration;Trusted_Connection=yes;')
-# print(pyodbc.drivers())
-# conn = pyodbc.connect('DRIVER={SQL Server};SERVER=DESKTOP-3H5IL08'+';DATABASE='+database+';Trusted_Connection=yes;'+'UID='+username+';PWD='+password+';')
 
-conn = mysql.connector.connect(host='localhost',database=database,user=username,password=password)
+# insert into uni_data(ranking,title,country,city,students,stu_staff_rt,intl_stu,gen_rt) values(1,'Trial','trial','trial',100,10.5,10.5,'2:2:30');
+
+
 
 CORS(app)
 @app.route('/home',methods=['GET'])
 def getData():
-    
+    extract_load()
+    conn = mysql.connector.connect(host='localhost',database=database,user=username,password=password)
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM student_data')
     row_headers=[x[0] for x in cursor.description] #this will extract row headers
@@ -30,6 +31,37 @@ def getData():
 @app.route('/home/stuData',methods=['GET'])
 def getStuData():
     return "Data!"
+
+def extract_load():
+    try:
+        conn = mysql.connector.connect(host='localhost',database=database,user=username,password=password)
+        uniData = pd.read_csv("universities_ranking_final.csv", delimiter = ',',error_bad_lines=False)
+        uniData.head()
+        if conn.is_connected():
+            cursor = conn.cursor()
+            for i,row in uniData.iterrows():
+                row[0] = int(row[0])
+                row[7] = str(row[7])
+                if row[7]=='nan': row[7] = ""
+                row[4] = row[4].replace("\"","")
+                row[4] = int(row[4].replace(",",""))
+                try:
+                    row[6] = row[6].replace("%","")
+                    if(row[6] != ""):
+                        row[6] = float(row[6])
+                except:
+                    print(row)
+
+                query = "insert into uni_data(ranking,title,country,city,students,stu_staff_rt,intl_stu,gen_rt) values(%s,%s,%s,%s,%s,%s,%s,%s)"
+                try:
+                    cursor.execute(query,tuple(row))
+                except Error as e:
+                    print("Record Insertion failed for: "+ str(row[0]) + " with error: "+ str(e))
+                conn.commit()
+        conn.close()
+    except:
+        print({}).format(e)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
